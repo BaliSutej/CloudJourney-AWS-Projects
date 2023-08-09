eed8fd87edc7dff11a63f0bc3bde2cb29981165c
sutej bali <sutejbali@gmail.com>
2023-08-09 17:21:24 +0530

    refactor: file rename

---
+++ apigateway/apigateway-custom-lambda-auth/src/tokenAuthFunction/app.js
@@ -0,0 +1,72 @@
+
+
+exports.handler = async (event, context) => {
+
+    // let token = event.authorizationToken;
+    let userId = 1;
+    // console.log("******TOKEN********");
+    // console.log(token);
+
+    console.log(event);
+
+    // const accessToken = token.replace("Bearer ", "");
+
+    // Fix for Authorizer 403 Error - https://aws.amazon.com/premiumsupport/knowledge-center/api-gateway-lambda-authorization-errors/
+    // TODO Change the variable naming if this fixes
+    const tmp = event.methodArn.split(":");
+    const apiGatewayArnTmp = tmp[5].split("/");
+
+    // Create wildcard resource
+    const resource =
+        tmp[0] +
+        ":" +
+        tmp[1] +
+        ":" +
+        tmp[2] +
+        ":" +
+        tmp[3] +
+        ":" +
+        tmp[4] +
+        ":" +
+        apiGatewayArnTmp[0] +
+        "/*/*";
+
+    let payload;
+    try {
+
+        // if () {
+        //     return generatePolicy("user", "Deny", resource);
+        // }
+        // userId = userDBResponse.data.id;
+    } catch (e) {
+        // API Gateway wants this *exact* error message, otherwise it returns 500 instead of 401:
+        console.log(e);
+        throw new Error("Unauthorized");
+    }
+
+    return generatePolicy("user", "Allow", resource, userId);
+};
+
+// Help function to generate an IAM policy
+var generatePolicy = function (principalId, effect, resource, userId) {
+    var authResponse = {};
+
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        const policyDocument = {};
+        policyDocument.Version = "2012-10-17";
+        policyDocument.Statement = [];
+        const statementOne = {};
+        statementOne.Action = "execute-api:Invoke";
+        statementOne.Effect = effect;
+        statementOne.Resource = resource;
+        policyDocument.Statement[0] = statementOne;
+        authResponse.policyDocument = policyDocument;
+    }
+
+    // Optional output with custom properties of the String, Number or Boolean type.
+    authResponse.context = {
+        userId: userId
+    };
+    return authResponse;
+};
---
+++ apigateway/apigateway-custom-lambda-auth/src/user/app.js
@@ -0,0 +1,24 @@
+exports.handler = async (event, context) => {
+    console.log("LAMBDA FUNCTION INVOKED");
+    console.log("************************");
+    console.log("EVENT");
+    console.log(event);
+    console.log("************************");
+    console.log("CONTEXT");
+    console.log(context);
+
+    return buildResponse(200, { message: "Lambda Successfully Invoked" })
+};
+
+
+function buildResponse(statusCode, body) {
+    return {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*"
+        },
+        body: JSON.stringify(body)
+    };
+} 
